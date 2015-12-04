aa3f955cc7a1f2c8daa26868751babddfc04bee2
Jamie King <king0120@gmail.com>
2015-12-03 19:14:29 -0500

    Completed up to problem 3

--- js_intro_and_control_flow.js
+++ js_intro_and_control_flow.js
@@ -1,10 +1,10 @@
-Intro to Javascript
+// Intro to Javascript
 
-1.) Find the last name in the following array:
+// 1.) Find the last name in the following array:
 
 var friends = [
-                'Moe', 
-                'Larry', 
+                'Moe',
+                'Larry',
                 'Curly',
                 'Jane',
                 'Emma',
@@ -17,7 +17,28 @@ var friends = [
                 'Harriet'
               ];
 
-Add your name to the end of the `friends` and add another name to beginning. Change the `Elizabeth` to `Liz`.
+// Add your name to the end of the `friends` and add another name to beginning. Change the `Elizabeth` to `Liz`.
+
+var lastName = friends[(friends.length-1)];
+
+friends.push("Jamie");
+
+friends.unshift("Tom");
+
+friends.splice(6, 1, "Liz");
+
+console.log(friends);
+console.log(friends.length);
+
+
+
+
+
+
+// 2.) Go to MDN and lookup `Array.sort`. Sort the list of `friends` above
+
+friends.sort();
+console.log(friends);
 
 
 
@@ -30,38 +51,31 @@ Add your name to the end of the `friends` and add another name to beginning. Cha
 
 
 
-2.) Go to MDN and lookup `Array.sort`. Sort the list of `friends` above
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-3.) Challenge Problem: Here are a list of `ages`
+// 3.) Challenge Problem: Here are a list of `ages`
 
 var ages = [83, 53, 37, 29, 60, 30, 66, 19, 59, 41, 9, 64, 19, 80, 24, 53, 70, 1, 53, 40, 92, 4, 71, 65, 8, 2, 51, 80, 94, 37, 80, 64, 19, 6, 14];
 
-Find the `median` age. Note: the median is the middle index of the sorted array. (Hint: Lookup up on MDN `Array.sort` for numbers, and use the length of the Ages.)
+// Find the `median` age. Note: the median is the middle index of the sorted array. (Hint: Lookup up on MDN `Array.sort` for numbers, and use the length of the Ages.)
+
+ages.sort(function (a,b) {return a - b;});
+
+function median(arr){
+  if(arr%2!==0){
+    console.log("odd");
+    return (arr[Math.ceil((arr.length-1)/2)] + arr[Math.floor((arr.length-1)/2)])/2;
+  } else {
+    console.log("even");
+    return arr[arr.length-1/2];
+  }
+}
+
+console.log(median(ages));
 
 
 
+// 4.) There are a list of names in a string, below. How could we sort them? Hint: use string and array methods.
 
-
-
-
-
-
-4.) There are a list of names in a string, below. How could we sort them? Hint: use string and array methods.
- 
 var friends = "Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,Harriet";
 
 
@@ -72,7 +86,7 @@ var friends = "Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,
 
 
 
-5.) List all the `friends` above in reverse alphabetical order.
+// 5.) List all the `friends` above in reverse alphabetical order.
 
 
 
@@ -83,7 +97,7 @@ var friends = "Moe,Larry,Curly,Jane,Emma,Elizabeth,Elinor,Mary,Darcy,Grey,Lydia,
 
 
 
-6.) We have two lists of friends below. We need to combine them into one list and sort them:
+// 6.) We have two lists of friends below. We need to combine them into one list and sort them:
 
 var myFriends = [
                   'Rickon',
@@ -118,7 +132,7 @@ var yourFriends = [
 
 
 
-7.) I have a list of favorite foods below. If `Popcorn` is my favorite food and `Potato chips` my second favorite, then how would you find the rank of another food. Hint: go to MDN to find an array method for finding the index of something in the array.
+// 7.) I have a list of favorite foods below. If `Popcorn` is my favorite food and `Potato chips` my second favorite, then how would you find the rank of another food. Hint: go to MDN to find an array method for finding the index of something in the array.
 
 var foods = [
               'Popcorn',
@@ -151,7 +165,7 @@ var foods = [
 
 
 
-8.) I made a mistake with my favorite foods. How can I find the index of `Donuts` and remove it? **(Hint: look up `splice`)**
+// 8.) I made a mistake with my favorite foods. How can I find the index of `Donuts` and remove it? **(Hint: look up `splice`)**
 
 
 
@@ -161,7 +175,7 @@ var foods = [
 
 
 
-9.) My friends want to know what my `5`th to `10`th favorite foods are.
+// 9.) My friends want to know what my `5`th to `10`th favorite foods are.
 
 
 
@@ -172,35 +186,35 @@ var foods = [
 
 
 
-10.) How would you create an array that efficiently stored the following information:
+// 10.) How would you create an array that efficiently stored the following information:
 
-  'Moe' is 18
-  'Larry' is 19
-  'Curly' is 20
-  'Jane' is 20
-  'Emma' is 21
-  'Elizabeth' is 18
-  'Elinor' is 23
-  'Mary' is 25
-  'Darcy' is 24
-  'Grey' is 18
-  'Lydia' is 24
-  'Harriet' is 18
+  // 'Moe' is 18
+  // 'Larry' is 19
+  // 'Curly' is 20
+  // 'Jane' is 20
+  // 'Emma' is 21
+  // 'Elizabeth' is 18
+  // 'Elinor' is 23
+  // 'Mary' is 25
+  // 'Darcy' is 24
+  // 'Grey' is 18
+  // 'Lydia' is 24
+  // 'Harriet' is 18
 
 
 
 
 
-##Javascript Control Flow Exercises
+// ##Javascript Control Flow Exercises
 
-1.) Nightclub Bouncer
+// 1.) Nightclub Bouncer
 
-Using an if/else expression, create a script that prompts the user for their age, then:
+// Using an if/else expression, create a script that prompts the user for their age, then:
 
-  * If the user is older than 18, he/she should receive a message that he/she may enter.
-  * If the user is less than 18, then he/she can't enter and should have a message telling him/her that he/she's too young.
-  * If the user is between 18 and 21, he/she should receive a message that he/she can enter, but not drink.
-  * If the user is older than 21, he/she should receive a message that he/she can both enter and drink.
+//   * If the user is older than 18, he/she should receive a message that he/she may enter.
+//   * If the user is less than 18, then he/she can't enter and should have a message telling him/her that he/she's too young.
+//   * If the user is between 18 and 21, he/she should receive a message that he/she can enter, but not drink.
+//   * If the user is older than 21, he/she should receive a message that he/she can both enter and drink.
 
 
 
@@ -213,10 +227,10 @@ Using an if/else expression, create a script that prompts the user for their age
 
 
 
-2.) Grade.js
-Output the following letter grade from a variable with with a test score. 
+// 2.) Grade.js
+// Output the following letter grade from a variable with with a test score.
 
-Based on the user input, display either "A", "B", "C", "D", or "F", for an score that is an integer between 0 and 100. Try and use a `switch` statement.
+// Based on the user input, display either "A", "B", "C", "D", or "F", for an score that is an integer between 0 and 100. Try and use a `switch` statement.
 
 
 
@@ -229,8 +243,8 @@ Based on the user input, display either "A", "B", "C", "D", or "F", for an score
 
 
 
-3.) Fizz Buzz
-Write a small program that asks a user for a number. If it's a multiple of 3, output "fizz". If it's a multiple of 5, output "buzz". If it's a multiple of 3 and 5, output "Fizzbuzz". 
+// 3.) Fizz Buzz
+// Write a small program that asks a user for a number. If it's a multiple of 3, output "fizz". If it's a multiple of 5, output "buzz". If it's a multiple of 3 and 5, output "Fizzbuzz".
 
 
 
@@ -243,8 +257,8 @@ Write a small program that asks a user for a number. If it's a multiple of 3, ou
 
 
 
-4.) BONUS!! (Not required, but give it the college try!)
+// 4.) BONUS!! (Not required, but give it the college try!)
 
-- There is an event where guests will be sitting in three sections based on their names: "left", "middle", and "right". If they have a premium ticket they can sit in first `3` rows in their section, otherwise they can take any seat behind row 3. Using hardcoded variables for `name` and `ticketType` print out appropriate seating instructions.
+// - There is an event where guests will be sitting in three sections based on their names: "left", "middle", and "right". If they have a premium ticket they can sit in first `3` rows in their section, otherwise they can take any seat behind row 3. Using hardcoded variables for `name` and `ticketType` print out appropriate seating instructions.
 
-- There is an event with ticket prices that are `$50`, `$65`, `$85` for standard, premier, and premier plus (for drinks) seating. Seniors, veterans, and students receive a `$10` discount while standard patrons  receive no discount. Based on hardcoded variables for `ticketType` and `discountType`, print out a patrons `ticketPrice`.
+// - There is an event with ticket prices that are `$50`, `$65`, `$85` for standard, premier, and premier plus (for drinks) seating. Seniors, veterans, and students receive a `$10` discount while standard patrons  receive no discount. Based on hardcoded variables for `ticketType` and `discountType`, print out a patrons `ticketPrice`.
