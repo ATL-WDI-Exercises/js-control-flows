70dc88a7ad5f628e45816a6422b1bb11e7e22a56
Chad Christensen <chadthecoder@gmail.com>
2015-12-05 08:19:19 -0500

    Added question 10

--- js_intro_and_control_flow.js
+++ js_intro_and_control_flow.js
@@ -183,18 +183,20 @@ console.log(foods.slice(4, 10));
 
 10.) How would you create an array that efficiently stored the following information:
 
-  'Moe' is 18
-  'Larry' is 19
-  'Curly' is 20
-  'Jane' is 20
-  'Emma' is 21
-  'Elizabeth' is 18
-  'Elinor' is 23
-  'Mary' is 25
-  'Darcy' is 24
-  'Grey' is 18
-  'Lydia' is 24
-  'Harriet' is 18
+var ages = {
+  'Moe' : 18,
+  'Larry' : 19,
+  'Curly' : 20,
+  'Jane' : 20,
+  'Emma' : 21,
+  'Elizabeth' : 18,
+  'Elinor' : 23,
+  'Mary' : 25,
+  'Darcy' : 24,
+  'Grey' : 18,
+  'Lydia' : 24,
+  'Harriet' : 18
+}
 
 
 
